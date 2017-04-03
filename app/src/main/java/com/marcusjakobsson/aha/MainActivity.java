f489e33aceec51a7f4f8218396d583f581ec1446
Ayko1595 <konstantin.ay95@gmail.com>
2017-04-03 13:06:32 +0200

    Added microphone button

--- app/src/main/java/com/marcusjakobsson/aha/MainActivity.java
+++ app/src/main/java/com/marcusjakobsson/aha/MainActivity.java
@@ -2,12 +2,23 @@ package com.marcusjakobsson.aha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn = (ImageButton) findViewById(R.id.button_mic);
+    }
+
+    public void micButton(View view)
+    {
+        Log.i("Mic", "Mic pressed");
     }
 }
