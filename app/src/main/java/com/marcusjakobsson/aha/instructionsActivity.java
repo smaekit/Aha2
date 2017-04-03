37c4203dbd4cb6cb30250803fcb3ec7d028e585f
Mäki-Dator\Mäki <Marcus.Jakobsson@live.se>
2017-04-03 19:17:14 +0200

    added a new Activity with a videoView

--- app/src/main/java/com/marcusjakobsson/aha/instructionsActivity.java
+++ app/src/main/java/com/marcusjakobsson/aha/instructionsActivity.java
@@ -3,7 +3,10 @@ package com.marcusjakobsson.aha;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class instructionsActivity extends AppCompatActivity {
 
@@ -16,6 +19,17 @@ public class instructionsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String msg = intent.getStringExtra("name");
         TextView textView =(TextView)findViewById(R.id.name_test);
-        textView.setText(msg);
+        textView.setText("Hej "+msg+ "!\nhär är en instruktionsvideo, starta den genom att trycka på play");
+
+        VideoView vv = (VideoView)findViewById(R.id.videoView);
+        vv.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.normal);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(vv);
+        ImageView imageView;
+
+
+        vv.setMediaController(mediaController);
+        vv.start();
     }
 }
