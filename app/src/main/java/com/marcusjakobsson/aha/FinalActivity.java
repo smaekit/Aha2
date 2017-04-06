64edbc05d1f27134da57ce6f2b07a2ebb852ae14
Ayko1595 <konstantin.ay95@gmail.com>
2017-04-06 16:03:30 +0200

    Made a final activity to hold the waiting screen (Not implemented).

---
+++ app/src/main/java/com/marcusjakobsson/aha/FinalActivity.java
@@ -0,0 +1,22 @@
+package com.marcusjakobsson.aha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class FinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final);
+
+    }
+
+}
