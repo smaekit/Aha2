43699c16cec4c5cf95a468947f56153664ce1bb7
Ayko1595 <konstantin.ay95@gmail.com>
2017-04-25 12:23:18 +0200

    Added refresh button

--- app/src/main/java/com/marcusjakobsson/aha/FinalActivity.java
+++ app/src/main/java/com/marcusjakobsson/aha/FinalActivity.java
@@ -1,29 +1,18 @@
 package com.marcusjakobsson.aha;
 
-
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
-
-import com.oralb.sdk.OBTBrushListener;
 import com.oralb.sdk.OBTSDK;
-import com.oralb.sdk.OBTSdkAuthorizationListener;
 
 public class FinalActivity extends MyOBTBrushListener{
 
     //TODO: Vid fel under uppkoppling till ORAL-Bs SDK, visa felmeddelande till användare
     //TODO: Erbjud möjligheten att försöka ansluta till ORAL-B på nytt
-    //TODO: Visa att man måste vara uppkopplad till Wi-fi
 
 
-    //TODO: Berätta för anv. att ansluta tandborste
+    //TODO: Berätta för anv. att ansluta tandborste (Vy)
     //TODO: Kolla om Wi-fi är på
     //TODO: Kolla om Bluetooth är på
     //TODO: Kolla om Tandborsten har kopplats, om den inte är så ska man via ett knapptryck koppla om och om igen
